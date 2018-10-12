521f50161c4a6de2ecac00feef87bd5250d70c0d
Sean Huber <seanhuber@seanhuber.com>
2018-10-12 09:48:52 -0500

    Support both Rails.application.credentials and Rails.application.secrets

--- lib/multi_session/session.rb
+++ lib/multi_session/session.rb
@@ -4,13 +4,13 @@ module MultiSession
       @cookies = cookies
     end
 
-    def [](key)
+    def [] key
       return nil unless @cookies[key.to_s].present?
-      session = ActiveSupport::JSON.decode encryptor(key).decrypt_and_verify(@cookies[key])
+      session = ActiveSupport::JSON.decode encryptor(key.to_s).decrypt_and_verify(@cookies[key.to_s])
       session['value'] # TODO: add ability to let developer retrieve the session_id
     end
 
-    def []=(key, value)
+    def []= key, value
       previous_session = self[key]
       session_id = if previous_session && previous_session['session_id'].present?
         previous_session['session_id']
@@ -18,12 +18,14 @@ module MultiSession
         SecureRandom.hex(16).encode Encoding::UTF_8
       end
 
-      new_session = {
-        'session_id' => session_id,
-        'value' => value
-      }
-      expiry_options = MultiSession.expires.present? ? {expires_at: Time.now + MultiSession.expires} : {}
-      encrypted_and_signed_value = encryptor(key).encrypt_and_sign ActiveSupport::JSON.encode(new_session), expiry_options
+      new_session = {'session_id' => session_id, 'value' => value}
+      enc = encryptor key.to_s
+      if enc.method(:encrypt_and_sign).arity > 1 # check number of arguments for encrypt_and_sign (more than 1 means we're in Rails 5.2+ and can have expirable messages)
+        expiry_options = MultiSession.expires.present? ? {expires_at: Time.now + MultiSession.expires} : {}
+        encrypted_and_signed_value = enc.encrypt_and_sign ActiveSupport::JSON.encode(new_session), expiry_options
+      else
+        encrypted_and_signed_value = enc.encrypt_and_sign ActiveSupport::JSON.encode(new_session)
+      end
 
       raise ActionDispatch::Cookies::CookieOverflow if encrypted_and_signed_value.bytesize > ActionDispatch::Cookies::MAX_COOKIE_SIZE
 
@@ -36,22 +38,30 @@ module MultiSession
     end
 
     def update_expiration
-      Rails.application.credentials[:multi_session_keys].each_key do |key|
+      multi_session_keys.each_key do |key|
         self[key] = self[key] # decrypt and re-encrypt to force expires_at to update
       end
     end
 
     private
 
+    def multi_session_keys
+      keys = if Rails.application.respond_to? :credentials
+        Rails.application.credentials[:multi_session_keys]
+      else
+        Rails.application.secrets[:multi_session_keys]
+      end
+      keys.symbolize_keys
+    end
+
     def encryptor key
-      secret_key_base = Rails.application.credentials[:multi_session_keys][key.to_sym]
+      secret_key_base = multi_session_keys[key.to_sym]
       raise ArgumentError.new("Rails.application.credentials[:multi_session_keys][:'#{key}'] has not been set.") unless secret_key_base.present?
 
       encrypted_cookie_cipher = 'aes-256-gcm'
       key_generator = ActiveSupport::CachingKeyGenerator.new ActiveSupport::KeyGenerator.new(secret_key_base, iterations: 1000)
       key_len = ActiveSupport::MessageEncryptor.key_len encrypted_cookie_cipher
-      salt = 'authenticated encrypted cookie'
-      secret = key_generator.generate_key(MultiSession.authenticated_encrypted_cookie_salt, key_len)
+      secret = key_generator.generate_key MultiSession.authenticated_encrypted_cookie_salt, key_len
 
       ActiveSupport::MessageEncryptor.new secret, cipher: encrypted_cookie_cipher, serializer: ActiveSupport::MessageEncryptor::NullSerializer
     end
--- lib/multi_session/version.rb
+++ lib/multi_session/version.rb
@@ -1,3 +1,3 @@
 module MultiSession
-  VERSION = '1.1.0'
+  VERSION = '1.1.1'
 end
